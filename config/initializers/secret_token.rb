85aad8372d8547856d3160da129c332396587705
Brian Han <brianhan87usa@gmail.com>
2013-09-03 12:35:16 -0400

    First commit; gemfile setup, using rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = 'ccc5a367579f4388b551ea811ddf95147751ebf78fb9a1e53e49ff1d09e597166f5e77e4834a799a770547dd7788449ea76b2ad315d1c2478d7b9b34726a1d2e'
